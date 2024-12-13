a52e8ce49a802ea4cab0e7a396c40d04258666c0
mamu2119 <chaitanyapandya1985@gmail.com>
2024-12-13 21:03:49 +0530

    Add files via upload

---
+++ Form validation.js
@@ -0,0 +1,22 @@
+function validate(e) {
+    e.preventDefault();
+    const email = document.getElementById("email").value;
+    const pass = document.getElementById("password").value;
+    const age = document.getElementById("age").value;
+    const msgBox = document.getElementById("message");
+    let message = '';
+    if(email === '') {
+        message = 'Enter an email.';
+        msgBox.style.color = 'darkred';
+    } else if (pass === '') {
+        message = 'Enter a password.';
+        msgBox.style.color = 'red';
+    } else if (age === '') {
+        message = 'Enter your age.';
+        msgBox.style.color = 'orange';
+    } else {
+        message = 'Login successful!';
+        msgBox.style.color = 'green';
+    }
+    msgBox.innerText = message;
+}
